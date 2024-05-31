9a5911758d20ba345c064d5b97856fa9c296afcb
Raul Elizalde <raulei2323@gmail.com>
2024-05-31 15:00:31 -0600

    manejodelDOM/pizza content updated

---
+++ freecodecamp/manejodelDOM/pizza/app.js
@@ -0,0 +1,25 @@
+// const contenedor = document.getElementById('contenedor')
+// console.log(contenedor)
+//?retorna mucha informacion del elemento 
+// const contenedor = document.getElementById('contenedor')
+// console.log(contenedor.innerHTML)
+//? retorna la etructura del html que tiene
+
+// const titulo = document.getElementById('titulo')
+// console.log(typeof titulo)
+//?notar que el metodo getElemenetById retorna el elemento como un objeto
+//?se puede ver si imprimimos el tipo de dato del elemento que fue tomado por getElementById
+
+// const titulo = document.getElementById('titulo')
+// console.log(titulo.innerText)
+// //?retorna el texro interno y retorna como string
+
+// const titulo = document.getElementById('titulo')
+// console.log(titulo.tagName)
+//?retorna el tipo de etiqueta que tiene el elemento en el doc html
+
+
+
+
+
+
