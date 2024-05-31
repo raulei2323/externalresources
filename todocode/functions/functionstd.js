054449cef41019eed652374833326f3f11ae55aa
Raul Elizalde <raulei2323@gmail.com>
2024-05-31 15:20:58 -0600

    todocode/functions content added

---
+++ todocode/functions/functionstd.js
@@ -0,0 +1,72 @@
+
+// let num1 
+// let num2
+
+// function sumarnumeros (num1, num2) {
+    
+//     let result
+//     result = num1 + num2
+//     console.log(result)
+    
+//     return
+// }
+
+// sumarnumeros(10, 90)
+///////////////////////////////////////////////////////////////////////////////////////
+//?------Otra forma-----
+
+// let num1 
+// let num2
+
+// function sumarnumeros (num1, num2) {
+    
+//     let result
+//     result = num1 + num2
+    
+    
+//     return result
+// }
+
+// console.log(sumarnumeros(10, 90))
+////////////////////////////////////////////////////////////////////////////
+//?una tercera forma sin la variable del resultado-----
+
+function sumar (numero1, numero2) {
+
+    return numero1 + numero2
+    
+}
+console.log(sumar(67, 98))
+console.log(sumar(45, 38))
+console.log(sumar(4850, 3890732898))
+
+function saludar () {
+
+    return 'hola como estas'
+    
+}
+
+console.log(saludar())
+console.log(sumar(409888, 308437437438))
+
+//! una forma muy sintetica de hacer una funcion....
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//?asignando una variable a la funcion para despues imprimir la variable en vex de la funcion........
+// function sumar2 (numero12, numero22) {
+//     let resultado
+//     resultado = numero12 + numero22
+//     return resultado
+    
+// }
+
+// let sumarian = sumar2(67, 89)
+// console.log(sumarian)
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    
+
