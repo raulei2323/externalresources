d56cde0dad57d7417ae5c93b4832e92eb31dff99
Raul Elizalde <raulei2323@gmail.com>
2024-05-31 15:39:11 -0600

    various/arraysreview1 content added

---
+++ various/arraysreview1/arraysreview1.js
@@ -0,0 +1,15 @@
+const objetoConArray = {
+    nombre: "Objeto con array",
+    arrayDeUnobjeto: ['abeja', 'banana', 'camion', 'dado'],
+}
+
+const arrayAparte = objetoConArray.arrayDeUnobjeto
+for (let contador = 0; contador < arrayAparte.length; contador ++) {
+    console.log(arrayAparte[contador] + ' jaja')
+}
+//?con la constante arrrayAparte se puede hacer un codigo mas limpio
+
+
+
+
+
