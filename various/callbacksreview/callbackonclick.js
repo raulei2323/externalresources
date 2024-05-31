c832e0cb8fd1b78788e42709108022eb02340829
Raul Elizalde <raulei2323@gmail.com>
2024-05-31 15:43:50 -0600

    various/callbacksreview content added

---
+++ various/callbacksreview/callbackonclick.js
@@ -0,0 +1,4 @@
+document.getElementById("#btn").addEventListener('click', ()=> {
+    console.log('Esto de activa haciendo click en el boton con id = "btn"')
+    })
+
---
+++ various/callbacksreview/callbacksjsreview.js
@@ -0,0 +1,34 @@
+
+setTimeout(()=> {
+    console.log('Esto se imprime despues de 10000 milisegundos')
+},10000
+)
+
+setTimeout(()=> {
+    console.log('Esto se imprime despues de 12000 milisegundos')
+},12000
+)
+
+setTimeout(()=> {
+    console.log('Esto se imprime despues de 14000 milisegundos')
+},14000
+)
+
+setTimeout(()=> {
+    console.log('Esto se imprime despues de 16000 milisegundos')
+},16000
+)
+
+setTimeout(()=> {
+    console.log('Esto se imprime despues de 18000 milisegundos')
+},18000
+)
+
+setTimeout(()=> {
+    console.log('Esto se imprime despues de 20000 milisegundos')
+},20000
+)
+console.log('Esto se imprime de forma inmediata')
+
+
+///////
