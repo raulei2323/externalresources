1d95412aa86475effe16a43fa885f87827e880e2
Raul Elizalde <raulei2323@gmail.com>
2024-05-31 15:22:44 -0600

    todocode/vid09excs content added

---
+++ todocode/vid09excs/vid09excs.js
@@ -0,0 +1,30 @@
+const num01 = prompt('Ingresa un primer numero')
+const num02 = prompt('Ingresa un segundo numero')
+
+let result = num01 - num02
+
+
+
+result > 0 && (
+    console.log('es mayor a cero' + result % 2 == 0 ? 'Y el numero es par' : 'El numero es impar')
+    
+    
+    )
+
+result <= 0 && console.log('Es menor o igual a 0')
+
+//?forma correcta de hacer if sin else en ternarios '&&'  actua como un if y no tiene else
+//? y de tener un if dentro del if. lo puse en el console.log para ahorrarme un console.log
+
+
+
+
+
+
+
+
+
+
+//
+
+
